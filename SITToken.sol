215e34f3574dd755eabf473d6710d3fdcaafac00
shinjong kim <63417540+Spielmannkim@users.noreply.github.com>
2023-03-25 16:58:16 +0900

    Update SITToken.sol

--- SITToken.sol
+++ SITToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
+contract SITToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Sitdown", "SIT") {
         _mint(msg.sender, initialSupply);
     }
